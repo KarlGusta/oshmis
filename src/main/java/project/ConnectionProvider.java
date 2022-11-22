ebd00c00629813785eb6fcb618dc6d68381e7973
Annoh Karlgusta <annoh.karlgusta@students.jkuat.ac.ke>
2022-11-22 12:56:24 +0300

    Chore: Database connection
    This is the connection to the database for the project.

--- src/main/java/project/ConnectionProvider.java
+++ src/main/java/project/ConnectionProvider.java
@@ -1,5 +1,22 @@
 package project;
 
+import java.sql.*;
+
 public class ConnectionProvider {
 
+	public static Connection getCon()
+	{
+		try
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/oshmisdb", "root", "22-mysql-22");
+			
+			return con;
+		}
+		catch(Exception e)
+		{
+			System.out.print(e);
+			return null;
+		}
+	}
 }
